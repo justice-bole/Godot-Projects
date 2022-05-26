c0adbe9c8177038b02ecc4c1f43c2327a2b297e4
NetworkerLurker <78961523+NetworkerLurker@users.noreply.github.com>
2022-05-26 16:15:31 +0200

    Initial Commit
    Initial Godot tutorial projects

---
+++ autoload/Scene1.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_Button_pressed():
+	Global.goto_scene("res://Scene2.tscn")
+
---
+++ autoload/Scene2.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_Button_pressed():
+	Global.goto_scene("res://Scene1.tscn")
